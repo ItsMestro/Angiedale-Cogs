dc62b7d7fa59094c7bbb78b2024327408ecbdd4f
Mestro <itsmestro@gmail.com>
2020-11-10 05:53:22 +0100

    Add pressf to general.py

--- general/general.py
+++ general/general.py
@@ -6,8 +6,10 @@ from typing import Final
 import urllib.parse
 import aiohttp
 import discord
+import asyncio
 from redbot.core import commands
 from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
+from redbot.core.utils.common_filters import filter_mass_mentions
 from redbot.core.utils.chat_formatting import (
     bold,
     escape,
@@ -20,9 +22,11 @@ from redbot.core.utils.chat_formatting import (
 class General(commands.Cog):
     """General commands."""
 
-    def __init__(self):
+    def __init__(self, bot):
         super().__init__()
         self.stopwatches = {}
+        self.bot = bot
+        self.channels = {}
 
     async def red_delete_data_for_user(self, **kwargs):
         """ Nothing to delete """
@@ -385,3 +389,57 @@ class General(commands.Cog):
             await ctx.send(
                 _("No Urban Dictionary entries were found, or there was an error in the process.")
             )
+
+    @commands.command()
+    @commands.bot_has_permissions(add_reactions=True)
+    async def pressf(self, ctx, *, user: discord.User = None):
+        """Pay respects by pressing F"""
+        if str(ctx.channel.id) in self.channels:
+            return await ctx.send(
+                "Oops! I'm still paying respects in this channel, you'll have to wait until I'm done."
+            )
+        self.channels[str(ctx.channel.id)] = {}
+
+        if user:
+            answer = user.display_name
+        else:
+            await ctx.send("What do you want to pay respects to?")
+
+            def check(m):
+                return m.author == ctx.author and m.channel == ctx.channel
+
+            try:
+                pressf = await ctx.bot.wait_for("message", timeout=120.0, check=check)
+            except asyncio.TimeoutError:
+                del self.channels[str(ctx.channel.id)]
+                return await ctx.send("You took too long to reply.")
+
+            answer = pressf.content[:1900]
+
+        message = await ctx.send(
+            f"Everyone, let's pay respects to **{filter_mass_mentions(answer)}**! Press the f reaction on the this message to pay respects."
+        )
+        await message.add_reaction("\U0001f1eb")
+        self.channels[str(ctx.channel.id)] = {"msg_id": message.id, "reacted": []}
+        await asyncio.sleep(120)
+        try:
+            await message.delete()
+        except (discord.errors.NotFound, discord.errors.Forbidden):
+            pass
+        amount = len(self.channels[str(ctx.channel.id)]["reacted"])
+        word = "person has" if amount == 1 else "people have"
+        await ctx.send(f"**{amount}** {word} paid respects to **{filter_mass_mentions(answer)}**.")
+        del self.channels[str(ctx.channel.id)]
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        if str(reaction.message.channel.id) not in self.channels:
+            return
+        if self.channels[str(reaction.message.channel.id)]["msg_id"] != reaction.message.id:
+            return
+        if user.id == self.bot.user.id:
+            return
+        if user.id not in self.channels[str(reaction.message.channel.id)]["reacted"]:
+            if str(reaction.emoji) == "\U0001f1eb":
+                await reaction.message.channel.send(f"**{user.name}** has paid their respects.")
+                self.channels[str(reaction.message.channel.id)]["reacted"].append(user.id)
