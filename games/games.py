4946968d5b4d51d840321635cc8581b21be737cc
Mestro <itsmestro@gmail.com>
2020-11-10 05:09:13 +0100

    Add Games Cog

---
+++ games/games.py
@@ -0,0 +1,179 @@
+import datetime
+import time
+from enum import Enum
+from random import randint, choice
+from typing import Final
+import urllib.parse
+import aiohttp
+import discord
+from redbot.core import commands
+from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
+from redbot.core.utils.chat_formatting import (
+    bold,
+    escape,
+    italics,
+    humanize_number,
+    humanize_timedelta,
+)
+
+
+class RPS(Enum):
+    rock = "\N{MOYAI}"
+    paper = "\N{PAGE FACING UP}"
+    scissors = "\N{BLACK SCISSORS}\N{VARIATION SELECTOR-16}"
+
+
+class RPSParser:
+    def __init__(self, argument):
+        argument = argument.lower()
+        if argument == "rock":
+            self.choice = RPS.rock
+        elif argument == "paper":
+            self.choice = RPS.paper
+        elif argument == "scissors":
+            self.choice = RPS.scissors
+        else:
+            self.choice = None
+
+
+MAX_ROLL: Final[int] = 2 ** 64 - 1
+
+
+class Games(commands.Cog):
+    """Collection of games for you to play."""
+
+    global _
+    _ = lambda s: s
+    ball = [
+        _("As I see it, yes"),
+        _("It is certain"),
+        _("It is decidedly so"),
+        _("Most likely"),
+        _("Outlook good"),
+        _("Signs point to yes"),
+        _("Without a doubt"),
+        _("Yes"),
+        _("Yes – definitely"),
+        _("You may rely on it"),
+        _("Reply hazy, try again"),
+        _("Ask again later"),
+        _("Better not tell you now"),
+        _("Cannot predict now"),
+        _("Concentrate and ask again"),
+        _("Don't count on it"),
+        _("My reply is no"),
+        _("My sources say no"),
+        _("Outlook not so good"),
+        _("Very doubtful"),
+    ]
+
+    def __init__(self):
+        super().__init__()
+
+    async def red_delete_data_for_user(self, **kwargs):
+        """ Nothing to delete """
+        return
+
+    @commands.command()
+    async def roll(self, ctx, number: int = 100):
+        """Roll a random number.
+
+        The result will be between 1 and `<number>`.
+
+        `<number>` defaults to 100.
+        """
+        author = ctx.author
+        if 1 < number <= MAX_ROLL:
+            n = randint(1, number)
+            await ctx.send(
+                "{author.mention} :game_die: {n} :game_die:".format(
+                    author=author, n=humanize_number(n)
+                )
+            )
+        elif number <= 1:
+            await ctx.send(_("{author.mention} Maybe higher than 1? ;P").format(author=author))
+        else:
+            await ctx.send(
+                _("{author.mention} Max allowed number is {maxamount}.").format(
+                    author=author, maxamount=humanize_number(MAX_ROLL)
+                )
+            )
+
+    @commands.command()
+    async def flip(self, ctx, user: discord.Member = None):
+        """Flip a coin... or a user.
+
+        Defaults to a coin.
+        """
+        if user is not None:
+            msg = ""
+            if user.id == ctx.bot.user.id:
+                user = ctx.author
+                msg = _("Nice try. You think this is funny?\n How about *this* instead:\n\n")
+            char = "abcdefghijklmnopqrstuvwxyz"
+            tran = "ɐqɔpǝɟƃɥᴉɾʞlɯuodbɹsʇnʌʍxʎz"
+            table = str.maketrans(char, tran)
+            name = user.display_name.translate(table)
+            char = char.upper()
+            tran = "∀qƆpƎℲפHIſʞ˥WNOԀQᴚS┴∩ΛMX⅄Z"
+            table = str.maketrans(char, tran)
+            name = name.translate(table)
+            await ctx.send(msg + "(╯°□°）╯︵ " + name[::-1])
+        else:
+            await ctx.send(_("*flips a coin and... ") + choice([_("HEADS!*"), _("TAILS!*")]))
+
+    @commands.command()
+    async def rps(self, ctx, your_choice: RPSParser):
+        """Play Rock Paper Scissors."""
+        author = ctx.author
+        player_choice = your_choice.choice
+        if not player_choice:
+            return await ctx.send(
+                _("This isn't a valid option. Try {r}, {p}, or {s}.").format(
+                    r="rock", p="paper", s="scissors"
+                )
+            )
+        red_choice = choice((RPS.rock, RPS.paper, RPS.scissors))
+        cond = {
+            (RPS.rock, RPS.paper): False,
+            (RPS.rock, RPS.scissors): True,
+            (RPS.paper, RPS.rock): True,
+            (RPS.paper, RPS.scissors): False,
+            (RPS.scissors, RPS.rock): False,
+            (RPS.scissors, RPS.paper): True,
+        }
+
+        if red_choice == player_choice:
+            outcome = None  # Tie
+        else:
+            outcome = cond[(player_choice, red_choice)]
+
+        if outcome is True:
+            await ctx.send(
+                _("{choice} You win {author.mention}!").format(
+                    choice=red_choice.value, author=author
+                )
+            )
+        elif outcome is False:
+            await ctx.send(
+                _("{choice} You lose {author.mention}!").format(
+                    choice=red_choice.value, author=author
+                )
+            )
+        else:
+            await ctx.send(
+                _("{choice} We're square {author.mention}!").format(
+                    choice=red_choice.value, author=author
+                )
+            )
+
+    @commands.command(name="8", aliases=["8ball"])
+    async def _8ball(self, ctx, *, question: str):
+        """Ask 8 ball a question.
+
+        Question must end with a question mark.
+        """
+        if question.endswith("?") and question != "?":
+            await ctx.send("`" + T_(choice(self.ball)) + "`")
+        else:
+            await ctx.send(_("That doesn't look like a question."))
