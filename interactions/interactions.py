7d4a0cc44ca3eed4a55b9128bfd285e7a6df456f
Mestro <itsmestro@gmail.com>
2020-11-12 05:48:16 +0100

    Add "lovers" to interactions.py

--- interactions/interactions.py
+++ interactions/interactions.py
@@ -1,6 +1,7 @@
 import discord
 from redbot.core import commands, Config
 from random import randint
+from bs4 import BeautifulSoup
 import aiohttp
 import logging
 
@@ -21,7 +22,9 @@ BaseCog = getattr(commands, "Cog", object)
 class Interactions(BaseCog):
     """Interact with people!"""
 
-    def __init__(self):
+    def __init__(self, bot):
+        super().__init__()
+        self.bot = bot
         self.config = Config.get_conf(self, identifier=1387002)
         default_global = {
             "hugs": [
@@ -533,3 +536,52 @@ class Interactions(BaseCog):
 
         if content["data"]["status"]["code"] == 200:
             return content["data"]["response"]["urls"]
+
+    @commands.command(aliases=["lovecalc", "lovercalculator"])
+    async def lovers(
+        self, ctx: commands.Context, lover: discord.Member, loved: discord.Member
+    ):
+        """Calculate the love percentage!"""
+
+        x = lover.display_name
+        y = loved.display_name
+
+        url = "https://www.lovecalculator.com/love.php?name1={}&name2={}".format(
+            x.replace(" ", "+"), y.replace(" ", "+")
+        )
+        async with aiohttp.ClientSession(headers={"Connection": "keep-alive"}) as session:
+            async with session.get(url, ssl=False) as response:
+                assert response.status == 200
+                resp = await response.text()
+
+        log.debug(f"{resp=}")
+        soup_object = BeautifulSoup(resp, "html.parser")
+
+        description = soup_object.find("div", class_="result__score").get_text()
+
+        if description is None:
+            description = "Dr. Love is busy right now"
+        else:
+            description = description.strip()
+
+        result_image = soup_object.find("img", class_="result__image").get("src")
+
+        result_text = soup_object.find("div", class_="result-text").get_text()
+        result_text = " ".join(result_text.split())
+
+        try:
+            z = description[:2]
+            z = int(z)
+            if z > 50:
+                emoji = "❤"
+            else:
+                emoji = "💔"
+            title = f"Dr. Love says that the love percentage for {x} and {y} is: {emoji} {description} {emoji}"
+        except (TypeError, ValueError):
+            title = "Dr. Love has left a note for you."
+
+        em = discord.Embed(
+            title=title, description=result_text, color=discord.Color.red(), url=url
+        )
+        em.set_image(url=f"https://www.lovecalculator.com/{result_image}")
+        await ctx.send(embed=em)
