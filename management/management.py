a507fa98fecf23dab21613cf664e112a58e5bb6b
Mestro <itsmestro@gmail.com>
2020-11-13 06:08:23 +0100

    Add "Say" to management.py

--- management/management.py
+++ management/management.py
@@ -17,6 +17,7 @@ from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
 from redbot.core.utils.mod import slow_deletion, mass_purge
 from redbot.core.utils.predicates import MessagePredicate
 from redbot.core.utils.chat_formatting import box, humanize_number, pagify
+from redbot.core.utils.tunnel import Tunnel
 from redbot.core.bot import Red
 from .alias_entry import AliasEntry, AliasCache, ArgParseError
 from .checks import check_self_permissions
@@ -63,6 +64,7 @@ class Management(commands.Cog):
     def __init__(self, bot: Red):
         super().__init__()
         self.bot = bot
+        self.interaction = []
         self.last_change = None
         self.config = Config.get_conf(self, 1387003, cog_name="ManagementAlias")
         self.config.register_global(entries=[], handled_string_creator=False)
@@ -85,6 +87,8 @@ class Management(commands.Cog):
 
     def cog_unload(self):
         self.presence_task.cancel()
+        for user in self.interaction:
+            self.bot.loop.create_task(self.stop_interaction(user))
 
     async def red_delete_data_for_user(
         self,
@@ -1338,3 +1342,182 @@ class Management(commands.Cog):
         elif len(new_statuses) == 1:
             return new_statuses[0]
         return current
+
+    async def say(
+        self, ctx: commands.Context, channel: Optional[discord.TextChannel], text: str, files: list
+    ):
+        if not channel:
+            channel = ctx.channel
+        if not text and not files:
+            await ctx.send_help()
+            return
+
+        # preparing context info in case of an error
+        if files != []:
+            error_message = (
+                "Has files: yes\n"
+                f"Number of files: {len(files)}\n"
+                f"Files URL: " + ", ".join([x.url for x in ctx.message.attachments])
+            )
+        else:
+            error_message = "Has files: no"
+
+        # sending the message
+        try:
+            await channel.send(text, files=files)
+        except discord.errors.HTTPException as e:
+            if not ctx.guild.me.permissions_in(channel).send_messages:
+                author = ctx.author
+                try:
+                    await ctx.send(
+                        _("I am not allowed to send messages in ") + channel.mention,
+                        delete_after=2,
+                    )
+                except discord.errors.Forbidden:
+                    await author.send(
+                        _("I am not allowed to send messages in ") + channel.mention,
+                        delete_after=15,
+                    )
+                    # If this fails then fuck the command author
+            elif not ctx.guild.me.permissions_in(channel).attach_files:
+                try:
+                    await ctx.send(
+                        _("I am not allowed to upload files in ") + channel.mention, delete_after=2
+                    )
+                except discord.errors.Forbidden:
+                    await author.send(
+                        _("I am not allowed to upload files in ") + channel.mention,
+                        delete_after=15,
+                    )
+            else:
+                log.error(
+                    f"Unknown permissions error when sending a message.\n{error_message}",
+                    exc_info=e,
+                )
+
+    @commands.command(name="say")
+    @checks.guildowner()
+    async def _say(self, ctx, channel: Optional[discord.TextChannel], *, text: str = ""):
+        """
+        Make the bot say what you want in the desired channel.
+
+        If no channel is specified, the message will be send in the current channel.
+        You can attach some files to upload them to Discord.
+
+        Example usage :
+        - `!say #general hello there`
+        - `!say owo I have a file` (a file is attached to the command message)
+        """
+
+        files = await Tunnel.files_from_attatch(ctx.message)
+        await self.say(ctx, channel, text, files)
+
+    @commands.command(name="sayd", aliases=["sd"])
+    @checks.guildowner()
+    async def _saydelete(self, ctx, channel: Optional[discord.TextChannel], *, text: str = ""):
+        """
+        Same as say command, except it deletes your message.
+
+        If the message wasn't removed, then I don't have enough permissions.
+        """
+
+        # download the files BEFORE deleting the message
+        author = ctx.author
+        files = await Tunnel.files_from_attatch(ctx.message)
+
+        try:
+            await ctx.message.delete()
+        except discord.errors.Forbidden:
+            try:
+                await ctx.send(_("Not enough permissions to delete messages."), delete_after=2)
+            except discord.errors.Forbidden:
+                await author.send(_("Not enough permissions to delete messages."), delete_after=15)
+
+        await self.say(ctx, channel, text, files)
+
+    @commands.command(name="interact")
+    @checks.guildowner()
+    async def _interact(self, ctx, channel: discord.TextChannel = None):
+        """Start receiving and sending messages as the bot through DM"""
+
+        u = ctx.author
+        if channel is None:
+            if isinstance(ctx.channel, discord.DMChannel):
+                await ctx.send(
+                    _(
+                        "You need to give a channel to enable this in DM. You can "
+                        "give the channel ID too."
+                    )
+                )
+                return
+            else:
+                channel = ctx.channel
+
+        if u in self.interaction:
+            await ctx.send(_("A session is already running."))
+            return
+
+        message = await u.send(
+            _(
+                "I will start sending you messages from {0}.\n"
+                "Just send me any message and I will send it in that channel.\n"
+                "React with ❌ on this message to end the session.\n"
+                "If no message was send or received in the last 5 minutes, "
+                "the request will time out and stop."
+            ).format(channel.mention)
+        )
+        await message.add_reaction("❌")
+        self.interaction.append(u)
+
+        while True:
+
+            if u not in self.interaction:
+                return
+
+            try:
+                message = await self.bot.wait_for("message", timeout=300)
+            except asyncio.TimeoutError:
+                await u.send(_("Request timed out. Session closed"))
+                self.interaction.remove(u)
+                return
+
+            if message.author == u and isinstance(message.channel, discord.DMChannel):
+                files = await Tunnel.files_from_attatch(message)
+                if message.content.startswith(tuple(await self.bot.get_valid_prefixes())):
+                    return
+                await channel.send(message.content, files=files)
+            elif (
+                message.channel != channel
+                or message.author == channel.guild.me
+                or message.author == u
+            ):
+                pass
+
+            else:
+                embed = discord.Embed()
+                embed.set_author(
+                    name="{} | {}".format(str(message.author), message.author.id),
+                    icon_url=message.author.avatar_url,
+                )
+                embed.set_footer(text=message.created_at.strftime("%d %b %Y %H:%M"))
+                embed.description = message.content
+                embed.colour = message.author.color
+
+                if message.attachments != []:
+                    embed.set_image(url=message.attachments[0].url)
+
+                await u.send(embed=embed)
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        if user in self.interaction:
+            channel = reaction.message.channel
+            if isinstance(channel, discord.DMChannel):
+                await self.stop_interaction(user)
+
+    async def stop_interaction(self, user):
+        self.interaction.remove(user)
+        await user.send(_("Session closed"))
+
+    def __unload(self):
+        self.cog_unload()
